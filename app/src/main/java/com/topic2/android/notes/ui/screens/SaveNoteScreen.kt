28ff010b2e958e2f2e48fcb2fd5578593d2f4fd2
notNero <kronat83@gmail.com>
2023-05-26 00:19:41 +0300

    Add function NoteCheckOption

--- app/src/main/java/com/topic2/android/notes/ui/screens/SaveNoteScreen.kt
+++ app/src/main/java/com/topic2/android/notes/ui/screens/SaveNoteScreen.kt
@@ -231,3 +231,30 @@ private fun PickedColor(color: ColorModel){
 fun PickedColorPreview(){
     PickedColor(ColorModel.DEFAULT)
 }
+
+@Composable
+private fun NoteCheckOption(
+    isChecked: Boolean,
+    onCheckedChange: (Boolean) -> Unit
+) {
+    Row(
+        Modifier
+            .padding(8.dp)
+            .padding(top = 16.dp)
+    ) {
+        Text(
+            text = "Can note be checked off?", modifier = Modifier.weight(1f)
+        )
+        Switch(
+            checked = isChecked,
+            onCheckedChange = onCheckedChange,
+            modifier = Modifier.padding(start = 8.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NoteCheckOptionPreview() {
+    NoteCheckOption(false) {}
+}
