fc11f44cfb3c7726296204dcd6afaecebd305e38
notNero <kronat83@gmail.com>
2023-05-25 17:53:11 +0300

    Change file NoteColor.kt

--- app/src/main/java/com/topic2/android/notes/ui/components/NoteColor.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/NoteColor.kt
@@ -1,28 +1,46 @@
 package com.topic2.android.notes.ui.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun NoteColor() {
+fun NoteColor(
+    color: Color,
+    size: Dp,
+    padding: Dp = 0.dp,
+    border: Dp
+){
     Box(
         modifier = Modifier
-            .size(40.dp)
-            .background(Color.Red)
+            .padding(padding)
+            .size(size)
             .clip(CircleShape)
+            .background(color)
+            .border(
+                BorderStroke(
+                    2.dp,
+                    SolidColor(Color.Black)
+                ),
+                CircleShape
+            )
     )
 }
 
 @Preview
 @Composable
 fun NoteColorPreview() {
-    NoteColor()
+    NoteColor(color = Color.Red, size = 40.dp, padding = 4.dp, border = 2.dp)
 }
